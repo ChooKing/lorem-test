e8d27ca92e0a183bb94cc15d34fcfd4c5c3178f8
Choo King <choocodes@gmail.com>
2025-06-06 16:27:02 -0400

    Implementation of lorem generator.

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,12 @@
-export const myFunction = () => {
-  return 'Hello, world!'
-}
+const BASE_TEXT = "One two three. Four five.";
+const WORDS = BASE_TEXT.split(" ");
+export const lorem = (length: number) => {
+	if (length <= WORDS.length) {
+		return WORDS.slice(0, length).join(" ");
+	}
+	const repetitions = Math.floor(length / WORDS.length);
+	const remainder = length % WORDS.length;
+	return (
+		`${BASE_TEXT} `.repeat(repetitions) + WORDS.slice(0, remainder).join(" ")
+	).trim();
+};
