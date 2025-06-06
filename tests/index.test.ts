e552d8fe6c78059d1b2f1ec24ba71315a5739c37
Choo King <choocodes@gmail.com>
2025-06-06 16:36:51 -0400

    Revised tests based on actual lorem text.

--- tests/index.test.ts
+++ tests/index.test.ts
@@ -1,8 +1,8 @@
-//All tests run with "One two three. Four five."
-//Actual lorem text not used for these tests. Synthetic text easier to analyze.
 import { expect, test } from 'vitest'
 import { lorem } from '../src'
-
+/*
+//These more comprehensive tests were done with "One two three. Four five."
+//Actual lorem text not used for these tests. Synthetic text easier to analyze.
 test("lorem2", () => {
 	expect(lorem(2)).toBe("One two");
 });
@@ -21,5 +21,14 @@ test("lorem10", () => {
 test("lorem14", () => {
 	expect(lorem(14)).toBe(
 		"One two three. Four five. One two three. Four five. One two three. Four",
+	);
+});
+*/
+test("lorem2", () => {
+	expect(lorem(2)).toBe("Lorem ipsum");
+});
+test("lorem8", () => {
+	expect(lorem(8)).toBe(
+		"Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
 	);
 });
